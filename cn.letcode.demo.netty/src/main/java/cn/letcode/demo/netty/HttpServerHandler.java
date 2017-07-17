38521a37f091fcf6471a55e3597272d2623ce341
kayak <chensj@letcode.cn>
2017-07-17 18:02:47 +0800

    update netty demo

--- cn.letcode.demo.netty/src/main/java/cn/letcode/demo/netty/HttpServerHandler.java
+++ cn.letcode.demo.netty/src/main/java/cn/letcode/demo/netty/HttpServerHandler.java
@@ -15,12 +15,38 @@
  */
 package cn.letcode.demo.netty;
 
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpHeaderValues;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpUtil;
+import io.netty.handler.codec.http.HttpVersion;
 
 public class HttpServerHandler extends ChannelInboundHandlerAdapter {
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		
+		if (msg instanceof HttpRequest) {
+			HttpRequest req = (HttpRequest) msg;
+			byte[] CONTENT = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd' };
+			boolean keepAlive = HttpUtil.isKeepAlive(req);
+			FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK,
+					Unpooled.wrappedBuffer(CONTENT));
+			// TODO
+			response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+			response.headers().set(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
+
+			if (!keepAlive) {
+				ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+			} else {
+				response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+				ctx.write(response);
+			}
+		}
 	}
 }
