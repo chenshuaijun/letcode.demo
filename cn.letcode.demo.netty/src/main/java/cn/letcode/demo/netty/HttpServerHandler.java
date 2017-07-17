2e364e4b2e625240392aa1d91e4cca75159ca973
kayak <chensj@letcode.cn>
2017-07-17 21:29:39 +0800

    update netty demo

--- cn.letcode.demo.netty/src/main/java/cn/letcode/demo/netty/HttpServerHandler.java
+++ cn.letcode.demo.netty/src/main/java/cn/letcode/demo/netty/HttpServerHandler.java
@@ -1,51 +1,58 @@
-/*
- * Copyright 2013 The Netty Project
- *
- * The Netty Project licenses this file to you under the Apache License,
- * version 2.0 (the "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at:
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
 package cn.letcode.demo.netty;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpHeaderValues;
-import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpUtil;
-import io.netty.handler.codec.http.HttpVersion;
+import io.netty.util.CharsetUtil;
 
+/**
+ * netty http server handler data deal
+ * 
+ * @author chenshuaijun
+ *
+ */
 public class HttpServerHandler extends ChannelInboundHandlerAdapter {
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) {
+		ctx.flush();
+	}
+
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		if (msg instanceof HttpRequest) {
-			HttpRequest req = (HttpRequest) msg;
-			byte[] CONTENT = { 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd' };
-			boolean keepAlive = HttpUtil.isKeepAlive(req);
-			FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK,
-					Unpooled.wrappedBuffer(CONTENT));
-			// TODO
-			response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
-			response.headers().set(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
+		if (msg instanceof FullHttpRequest) {
+			FullHttpRequest req = (FullHttpRequest) msg;
+			req.protocolVersion();
+			// TODO check content_type swich doing
+			if (HttpHeaderValues.APPLICATION_JSON.contentEquals(req.headers().get(HttpHeaderNames.CONTENT_TYPE))) {
+				System.out.println("true===true");
+			}
 
+			ByteBuf bb = req.content();
+			String content = bb.toString(CharsetUtil.UTF_8);
+			System.out.println(content);
+
+			boolean keepAlive = HttpUtil.isKeepAlive(req);
+			// http version from http request
+			FullHttpResponse rep = new DefaultFullHttpResponse(req.protocolVersion(), HttpResponseStatus.OK,
+					Unpooled.wrappedBuffer(content.getBytes(CharsetUtil.UTF_8)));
+
+			req.headers().set(HttpHeaderNames.CONTENT_TYPE, "");
+			rep.headers().set(HttpHeaderNames.CONTENT_LENGTH, rep.content().readableBytes());
 			if (!keepAlive) {
-				ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+				ctx.write(rep).addListener(ChannelFutureListener.CLOSE);
 			} else {
-				response.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
-				ctx.write(response);
+				rep.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+				ctx.write(rep);
 			}
 		}
 	}
--- cn.letcode.demo.netty/src/main/java/cn/letcode/demo/netty/HttpServerInitializer.java
+++ cn.letcode.demo.netty/src/main/java/cn/letcode/demo/netty/HttpServerInitializer.java
@@ -3,17 +3,36 @@ package cn.letcode.demo.netty;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.http.HttpRequestDecoder;
-import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
 
 public class HttpServerInitializer extends ChannelInitializer<SocketChannel> {
 
 	@Override
 	protected void initChannel(SocketChannel socketChannel) throws Exception {
 		ChannelPipeline p = socketChannel.pipeline();
-		p.addLast("http.decoder", new HttpRequestDecoder());
-		p.addLast("http.encoder", new HttpResponseEncoder());
+		/**
+		 * http-request解码器 http服务器端对request解码
+		 */
+		// p.addLast(new HttpRequestDecoder());
+		/**
+		 * http-response解码器 http服务器端对response编码
+		 */
+		// p.addLast(new HttpResponseEncoder());
+		/**
+		 * 压缩 Compresses an HttpMessage and an HttpContent in gzip or deflate
+		 * encoding while respecting the "Accept-Encoding" header. If there is
+		 * no matching encoding, no compression is done.
+		 */
+		// p.addLast("deflater", new HttpContentCompressor());
+		// p.addLast(new JsonObjectDecoder());
+
+		p.addLast(new HttpServerCodec());
+		// HttpObjectAggregator会把多个消息转换为一个单一的FullHttpRequest或是FullHttpResponse
+		p.addLast(new HttpObjectAggregator(65536));
+
 		p.addLast(new HttpServerHandler());
+
 	}
 
 }
